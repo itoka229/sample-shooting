9d0eed53a5c3e775823d2fe97d190604d4733dea
DESKTOP-APK21IT\itoka229 <itoka229@yahoo.co.jp>
2018-05-31 19:57:01 +0900

    射撃部分、的の追加 新規プロジェクトの作成

---
+++ Assets/RotateCube.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour {
+
+	// 
+	void Start () {
+		
+	}
+	
+	// Cubeの回転
+	void Update () {
+        //→キーが押されたとき
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            transform.Rotate(new Vector3(0, 45, 0));
+        }
+        //←キーが押されたとき
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, -45, 0));
+        }
+    }
+}
