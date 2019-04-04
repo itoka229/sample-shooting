cc4815d13b1af43d0b8f9da7d8aa521f5e56e319
DESKTOP-APK21IT\itoka229 <itoka229@yahoo.co.jp>
2019-04-04 12:02:54 +0900

    fontの追加、位置の調整

--- Assets/script/start/startbutton.cs
+++ Assets/script/start/startbutton.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class startbutton : MonoBehaviour {
-    
+
     //ボタンを押したらゲーム画面に移動する。
     public void OnClick()
     {
 
         SceneManager.LoadScene("waiting");
 
-    }    
+    }
+    void Update()
+    {
+    if(Input.GetKeyDown(KeyCode.Z))
+        {
+            SceneManager.LoadScene("waiting");
+        }
+    }
+
+
 }
+
